edb1d8a7469eea662b2d3c47abce69f514690c0d
bartek537 <60577942+bartek537@users.noreply.github.com>
2026-03-09 21:59:47 +0100

    Define Student, Address and CollegeLevel models

---
+++ Laboratory_20260309/Domain/Models/CollegeLevel.cs
@@ -0,0 +1,8 @@
+﻿namespace Laboratory_20260309.Domain.Models;
+
+internal enum CollegeLevel
+{
+    I = 1,
+    II = 2,
+    III = 3
+}
---
+++ Laboratory_20260309/Domain/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace Laboratory_20260309.Domain.Models;
+
+internal class Student
+{
+    public Guid Id { get; } = Guid.NewGuid();
+
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+
+    public required DateTime BirthDate { get; init; }
+    
+    public required Address HomeAddress { get; set; }
+
+    public CollegeLevel CollegeLevel { get; set; }
+
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName} (ID: {Id})";
+    }
+}
