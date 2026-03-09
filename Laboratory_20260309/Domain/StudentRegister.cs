5ca35fcd899d98dd22f2b206e609b719fd2c8062
bartek537 <60577942+bartek537@users.noreply.github.com>
2026-03-10 00:18:45 +0100

    Define StudentRegister

---
+++ Laboratory_20260309/Domain/StudentRegister.cs
@@ -0,0 +1,23 @@
+﻿using Laboratory_20260309.Domain.Models;
+using System.ComponentModel;
+
+namespace Laboratory_20260309.Domain;
+
+internal class StudentRegister
+{
+    public BindingList<Student> Students { get; } = [];
+
+    public void AddStudent(Student student)
+    {
+        if (Students.Contains(student))
+        {
+            return;
+        }
+        Students.Add(student);
+    }
+
+    public void RemoveStudent(Student student)
+    {
+        Students.Remove(student);
+    }
+}
