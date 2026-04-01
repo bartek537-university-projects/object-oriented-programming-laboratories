aa3ab4ba5244c038e0991107a761935dacaa496c
bartek537 <60577942+bartek537@users.noreply.github.com>
2026-04-01 14:58:27 +0200

    Create IValidator interface and ValidationException

---
+++ Laboratory_20260323/Application/Abstractions/Interfaces/IValidator.cs
@@ -0,0 +1,6 @@
+﻿namespace Laboratory_20260323.Application.Abstractions.Interfaces;
+
+public interface IValidator<T>
+{
+    Dictionary<string, string> Validate(T value);
+}
---
+++ Laboratory_20260323/Application/Common/Exceptions/ValidationException.cs
@@ -0,0 +1,6 @@
+﻿namespace Laboratory_20260323.Application.Common.Exceptions;
+
+public class ValidationException(IReadOnlyDictionary<string, string> errors) : Exception
+{
+    public IReadOnlyDictionary<string, string> Errors { get; } = errors;
+}
