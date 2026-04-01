c249ef6ff175e99665dfe5e2966716eb144e1673
bartek537 <60577942+bartek537@users.noreply.github.com>
2026-04-01 13:55:27 +0200

    Create AddEmployee use case

---
+++ Laboratory_20260323/Application/Employees/AddEmployee.cs
@@ -0,0 +1,21 @@
+﻿namespace Laboratory_20260323.Application.Employees;
+
+public class AddEmployee
+{
+    public record Request(string FirstName, string LastName);
+
+    public class Handler : IAddEmployeeHandler
+    {
+        public Response Handle(Request request)
+        {
+            return new Response();
+        }
+    }
+
+    public record Response();
+}
+
+public interface IAddEmployeeHandler
+{
+    AddEmployee.Response Handle(AddEmployee.Request request);
+}
