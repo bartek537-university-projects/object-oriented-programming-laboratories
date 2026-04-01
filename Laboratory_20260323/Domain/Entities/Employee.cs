3279e1f1451873e68f3ee3182a941736d1a322a7
bartek537 <60577942+bartek537@users.noreply.github.com>
2026-04-01 14:57:47 +0200

    Create employee domain model

---
+++ Laboratory_20260323/Domain/Entities/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace Laboratory_20260323.Domain.Entities;
+
+public class Employee
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}
