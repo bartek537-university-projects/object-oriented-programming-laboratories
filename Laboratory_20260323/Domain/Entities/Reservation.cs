eb126fac579f5f6249e37862722fcf956b912c4f
bartek537 <60577942+bartek537@users.noreply.github.com>
2026-04-06 17:24:20 +0200

    Create reservation model

---
+++ Laboratory_20260323/Domain/Entities/Reservation.cs
@@ -0,0 +1,10 @@
+﻿namespace Laboratory_20260323.Domain.Entities;
+
+public class Reservation
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public required Room Room { get; set; }
+    public required Employee Employee { get; set; }
+    public required DateTime Start { get; set; }
+    public required DateTime End { get; set; }
+}
