29ae73f6d70b5eebbcdcfde44549b9fb543c3f58
bartek537 <60577942+bartek537@users.noreply.github.com>
2026-04-06 17:29:12 +0200

    Define IRequest and IRequestHandler to improve use-case code

---
+++ Laboratory_20260323/Application/Abstractions/Interfaces/IRequest.cs
@@ -0,0 +1,5 @@
+﻿namespace Laboratory_20260323.Application.Abstractions.Interfaces;
+
+public interface IRequest<TResponse>
+{
+}
---
+++ Laboratory_20260323/Application/Abstractions/Interfaces/IRequestHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace Laboratory_20260323.Application.Abstractions.Interfaces;
+
+public interface IRequestHandler<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    TResponse Handle(TRequest request);
+}
+
