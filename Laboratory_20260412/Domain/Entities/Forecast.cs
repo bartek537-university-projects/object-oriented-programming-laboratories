00d6481ce7cdaa736c4b13997955505c6155726c
bartek537 <60577942+bartek537@users.noreply.github.com>
2026-04-17 20:21:45 +0200

    Create domain models for Forecast and Geolocation

---
+++ Laboratory_20260412/Domain/Entities/Forecast.cs
@@ -0,0 +1,27 @@
+﻿namespace Laboratory_20260412.Domain.Entities;
+
+public record Forecast(
+    Guid Id,
+    WeatherConditions Weather,
+    TemperatureConditions Temperature,
+    AtmosphericConditions Atmospheric
+);
+
+public enum WeatherConditions
+{
+    Thunderstorm,
+    Drizzle,
+    Rain,
+    Snow,
+    Mist,
+    Clear,
+    Clouds,
+}
+
+public record TemperatureConditions(
+    double Current, double Minimum, double Maximum
+);
+
+public record AtmosphericConditions(
+    double Pressure, double Humidity
+);
---
+++ Laboratory_20260412/Domain/Entities/Geolocation.cs
@@ -0,0 +1,3 @@
+﻿namespace Laboratory_20260412.Domain.Entities;
+
+public record class Geolocation(double Latitude, double Longitude);
