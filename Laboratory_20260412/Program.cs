5907bd900123edce4d11f35473ae067c778cc681
bartek537 <60577942+bartek537@users.noreply.github.com>
2026-04-18 20:31:26 +0200

    Setup appconfig.json

--- Laboratory_20260412/Program.cs
+++ Laboratory_20260412/Program.cs
@@ -1,13 +1,16 @@
 using Laboratory_20260412.Presentation.Main;
+using Microsoft.Extensions.Configuration;
 
-namespace Laboratory_20260412
-{
+namespace Laboratory_20260412;
+
     internal static class Program
     {
         [STAThread]
         private static void Main()
         {
             ApplicationConfiguration.Initialize();
+
+        IConfiguration configuration = GetConfiguration();
 
             MainForm view = new();
             MainPresenter presenter = new(view);
@@ -16,5 +19,13 @@ namespace Laboratory_20260412
 
             System.Windows.Forms.Application.Run(view);
         }
+
+    private static IConfiguration GetConfiguration()
+    {
+        IConfigurationBuilder builder = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appconfig.json", optional: false);
+
+        return builder.Build();
     }
 }
